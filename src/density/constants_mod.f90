50fdde5687e888aa74a8d164cdfc7a3756ef9aa3
gianluca <gianluca.bertaina@zoho.com>
2020-03-17 18:48:52 +0100

    clean constants file

--- src/density/constants_mod.f90
+++ src/density/constants_mod.f90
@@ -15,22 +15,4 @@
        real(8)   , parameter  :: zero_real       = 0.d0
        complex(8), parameter  :: aye             = (0.d0,1.d0)
 
-
-! DA AGGIUSTARE PERCHE' POCO CHIARE: (DA CHIEDERE)
-       real(8)   , parameter  :: toautime   = 4.1341d4
-       real(8)   , parameter  :: toauhess   = 1.066D-6
-       real(8)   , parameter  :: toaueng    = 3.8088d-4
-
-
-
-! old constants: 
-!      toev = 27.2114d0
-!      tocm = 219474.625d0
-!      toau = 1.d0/0.529177d0
-!      toauvel = 1.d0/21.87676d0
-!      toaueng = 3.8088d-4
-
-
-
-
       END MODULE constants
