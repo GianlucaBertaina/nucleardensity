7b4b1481e8a6db58ba8d80f130f31a8282a5bc64
gianluca <gianluca.bertaina@zoho.com>
2020-03-20 14:47:17 +0100

    do no track output and cube files. Adds vmd files for visualization

--- run_Glyp/evaluate.sh
+++ run_Glyp/evaluate.sh
@@ -3,9 +3,11 @@
 # Insert number of available CPU cores
 export NPROC=20
 
+export BASE=$(pwd)
+
 export natoms=11
-export DIRDENSITY=$(pwd)"/../src/density"
-export DIRCRUDE=$(pwd)"/../src/crude"
+export DIRDENSITY=${BASE}"/../src/density"
+export DIRCRUDE=${BASE}"/../src/crude"
 
 pushd $DIRDENSITY
 make all
@@ -20,23 +22,26 @@ for level in anharmonic harmonic; do
   pushd $level
   for state in ZPE OH; do
     pushd $state
+    rm *out *cube *gz expect*
     mpirun -n $NPROC $DIRDENSITY/nucleardensity |tee log.out
     popd
   done
   popd
 done
 
-# Evaluate totale density as sum of marginal densities. Archive marginal densities
+# Evaluate total density as sum of marginal densities. Archive marginal densities
 for level in anharmonic harmonic; do
   pushd $level
   for state in ZPE OH; do
     pushd $state
+    # Sum
     sumfile=density_sum_${level}_${state}.cube
     cp nucl_1.cube ${sumfile}
     for i in $(seq 2 ${natoms}); do
       $DIRCRUDE/crude comb ${sumfile} nucl_${i}.cube 1.0 1.0
       mv cuberes.cube  ${sumfile}
     done
+    # Archive
     tar -czvf nucl.tar.gz nucl_* ${sumfile}
     rm nucl_*.cube
     popd
@@ -44,8 +49,21 @@ for level in anharmonic harmonic; do
   popd
 done
 
+# Evaluate differences of total densities for visualization
+pushd visualize    
+# Harmonic OH minus Harmonic ZPE
+level=harmonic
+$DIRCRUDE/crude comb ${BASE}/$level/OH/density_sum_${level}_OH.cube ${BASE}/$level/ZPE/density_sum_${level}_ZPE.cube 1.0 -1.0; mv cuberes.cube density_diff_OH-ZPE_${level}.cube
+# Anharmonic OH minus Anharmonic ZPE
+level=anharmonic
+$DIRCRUDE/crude comb ${BASE}/$level/OH/density_sum_${level}_OH.cube ${BASE}/$level/ZPE/density_sum_${level}_ZPE.cube 1.0 -1.0; mv cuberes.cube density_diff_OH-ZPE_${level}.cube
+# Anharmonic ZPE minus Harmonic ZPE
+state=ZPE
+$DIRCRUDE/crude comb ${BASE}/anharmonic/$state/density_sum_anharmonic_${state}.cube ${BASE}/harmonic/$state/density_sum_harmonic_${state}.cube 1.0 -1.0; mv cuberes.cube density_diff_anharmonic-harmonic_${state}.cube
+# Anharmonic OH minus Harmonic OH
+state=OH
+$DIRCRUDE/crude comb ${BASE}/anharmonic/$state/density_sum_anharmonic_${state}.cube ${BASE}/harmonic/$state/density_sum_harmonic_${state}.cube 1.0 -1.0; mv cuberes.cube density_diff_anharmonic-harmonic_${state}.cube
+popd
+
 exit 0
 
-# archive cubes for wf
-for j in ${states}; do pushd wf-${j}-symm-gs-pruned;tar -czvf nucl.tar.gz nucl_* ; popd; done
-for j in ${states}; do pushd wf-${j}-symm-gs-pruned;rm nucl_{1..11}.cube ; popd; done
